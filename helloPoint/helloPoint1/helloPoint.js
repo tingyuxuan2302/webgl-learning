637d35a695c39a3501a6f62abd03648c587a237a
笙痞 <897411954@qq.com>
2023-03-03 15:59:38 +0800

    暂存

--- helloPoint/helloPoint1/helloPoint.js
+++ helloPoint/helloPoint1/helloPoint.js
@@ -6,26 +6,26 @@
  * @LastEditTime: 2023-02-27 22:46:47
  */
 
-// const VSHADER_SOURCE = `
-//   void main() {
-//     gl_Position = vec4(0.0, 0.0, 0.0, 1.0)
-//     gl_PointSize = 10.0
-//   }
-// `;
-// const FSHADER_SOURCE = `
-//   void main() {
-//     gl_FragColor = vec4(1.0,0.0, 0.0, 1.0 )
-//   }
-// `;
+const VSHADER_SOURCE = `
+  void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 10.0;
+  }
+`;
+const FSHADER_SOURCE = `
+  void main() {
+    gl_FragColor = vec4(1.0,0.0, 0.0, 1.0 );
+  }
+`;
 // 顶点着色器
-const VSHADER_SOURCE =
-  "void main() {\n" +
-  "gl_Position = vec4(0.5, 0.5, 0.0, 1.0);\n" +
-  "gl_PointSize = 10.0;\n" +
-  "}\n";
-// 片元着色器
-const FSHADER_SOURCE =
-  "void main() {\n" + "gl_FragColor = vec4(0.0,1.0, 0.0, 1.0);\n" + "}\n";
+// const VSHADER_SOURCE =
+//   "void main() {\n" +
+//   "gl_Position = vec4(0.5, 0.5, 0.0, 1.0);\n" +
+//   "gl_PointSize = 10.0;\n" +
+//   "}\n";
+// // 片元着色器
+// const FSHADER_SOURCE =
+//   "void main() {\n" + "gl_FragColor = vec4(0.0,1.0, 0.0, 1.0);\n" + "}\n";
 function main() {
   const canvas = document.getElementById("webgl-container");
   const gl = getWebGLContext(canvas);
